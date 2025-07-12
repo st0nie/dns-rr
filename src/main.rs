44cf7eafdb6ce7ad16574909dc350093d6f12256
ston <ston.jia@outlook.com>
2025-07-12 16:08:32 +0800

    cache invalid response

--- src/main.rs
+++ src/main.rs
@@ -110,25 +110,34 @@ async fn query_handler(
         }
 
         // 没有缓存就问上游
-        if let Ok(addrs) = tokio::net::lookup_host(format!("{}:0", qname_string)).await {
-            let v4_addrs = addrs
-                .filter_map(|addr| -> Option<Ipv4Addr> {
-                    if let SocketAddr::V4(addr_v4) = addr {
-                        reply.answers.push(ResourceRecord::new(
-                            qname.clone(),
-                            CLASS::IN,
-                            TTL,
-                            RData::A((*addr_v4.ip()).into()),
-                        ));
-                        Some(*addr_v4.ip())
-                    } else {
-                        None
-                    }
-                })
-                .collect::<Vec<_>>();
+        match tokio::net::lookup_host(format!("{}:0", qname_string)).await {
+            Ok(addrs) => {
+                let v4_addrs = addrs
+                    .filter_map(|addr| -> Option<Ipv4Addr> {
+                        if let SocketAddr::V4(addr_v4) = addr {
+                            reply.answers.push(ResourceRecord::new(
+                                qname.clone(),
+                                CLASS::IN,
+                                TTL,
+                                RData::A((*addr_v4.ip()).into()),
+                            ));
+                            Some(*addr_v4.ip())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<_>>();
 
-            dns_cache.insert(qname_string, v4_addrs, Duration::from_secs(TTL as u64));
-        }
+                dns_cache.insert(qname_string, v4_addrs, Duration::from_secs(TTL as u64));
+            }
+            // 如果上游也无法解析域名，记录缓存
+            Err(e) => {
+                // 11001 Host not found
+                if e.raw_os_error() == Some(11001) {
+                    dns_cache.insert(qname_string, Vec::new(), Duration::from_secs(TTL as u64));
+                }
+            }
+        };
     }
     socket.send_to(&reply.build_bytes_vec()?, addr).await?;
 
